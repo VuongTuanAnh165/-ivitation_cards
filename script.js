e84915b436ea127c5225196bf96c96a22e8808f8
VuongTuanAnh165 <92902120+VuongTuanAnh165@users.noreply.github.com>
2024-04-06 20:44:45 +0700

    đá

--- script.js
+++ script.js
@@ -9,19 +9,43 @@ $(document).ready(function () {
 			'avatar': 'image/26122001.jpg'
 		},
 		{
-			"id": 2,
-			"name": "Mít xinh đẹp"
+			"id": 24092001,
+			"pronouns": "bạn",
+			"name": "Mít xinh đẹp",
+			'avatar': 'image/24092001.jpg'
+		},
+		{
+			"id": 962006,
+			"pronouns": "con em xấu xa",
+			"name": "Ngọc còi",
+			'avatar': 'image/962006.jpg'
 		},
 	]
 
 	function getText() {
 		const queryString = window.location.search.replace("?", '');
 		const result = data.find(item => item.id === Number(queryString));
-		console.log(result)
 		$("#pronouns").text(result.pronouns)
 		$("#name").text(result.name)
 		$("#avatar").attr("src", result.avatar)
 	}
 
 	getText()
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var player = document.getElementById('player');
+
+    // Chờ sự kiện click trên trang web
+    document.addEventListener('click', function() {
+        // Phát nhạc khi có sự tương tác từ người dùng
+        player.play();
+        // Loại bỏ sự kiện click sau khi phát nhạc
+        document.removeEventListener('click', arguments.callee);
+    });
+    // Lặp lại phát nhạc khi kết thúc
+    player.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    });
 });
