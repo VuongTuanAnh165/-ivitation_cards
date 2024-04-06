761be35808d900eeec954ca35a395439fd7a4b0b
VuongTuanAnh165 <92902120+VuongTuanAnh165@users.noreply.github.com>
2024-04-06 19:44:12 +0700

    đá

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready(function () {
+	const data = [
+		{
+			"id": 26122001,
+			"pronouns": "mẹ già",
+			"name": "Phạm Thanh Tâm",
+			'img': 'image/26122001.jpg'
+		},
+		{
+			"id": 2,
+			"name": "Mít xinh đẹp"
+		},
+	]
+
+	function getText() {
+		const queryString = window.location.search.replace("?", '');
+		const result = data.find(item => item.id === Number(queryString));
+		console.log(result)
+		$("#pronouns").text(result.pronouns)
+		$("#name").text(result.name)
+	}
+
+	getText()
+});
